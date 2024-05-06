37e68488dc560125fab16ae7648971cddc4ed375
EpicZuko <zukokalilov5@gmail.com>
2024-05-06 21:56:00 +0600

    created card

---
+++ src/components/card/CardSaleSliderImg.js
@@ -0,0 +1,49 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable react/no-array-index-key */
+import React from 'react'
+import { Slide } from 'react-slideshow-image'
+import styled from 'styled-components'
+import 'react-slideshow-image/dist/styles.css'
+import { slideProperties } from '../../utils/constants/Url'
+
+const CardSaleSliderImg = ({ img, id }) => {
+  return (
+    <DIVSTYLED>
+      <Slide {...slideProperties} arrows={false}>
+        {Array.isArray(img) ? (
+          img.map((elem) => (
+            <div className='each-slide-effect' key={id}>
+              <div style={{ backgroundImage: `url(${elem})` }} />
+            </div>
+          ))
+        ) : (
+          <div
+            className='each-slide-effect'
+            style={{ backgroundImage: `url(${img})` }}
+          />
+        )}
+      </Slide>
+    </DIVSTYLED>
+  )
+}
+
+export default CardSaleSliderImg
+
+const DIVSTYLED = styled.div`
+  z-index: 1000;
+  .each-slide-effect > div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-size: cover;
+    height: 200px;
+    border-radius: 10px;
+  }
+
+  .each-slide-effect span {
+    padding: 20px;
+    font-size: 20px;
+    background: #efefef;
+    text-align: center;
+  }
+`
--- src/components/slider/SliderImg.js
+++ src/components/slider/SliderImg.js
@@ -6,7 +6,13 @@ import styled from 'styled-components'
 import 'react-slideshow-image/dist/styles.css'
 import { slideProperties } from '../../utils/constants/Url'
 
-const SliderImg = ({ images }) => {
+const SliderImg = () => {
+  const images = [
+    'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+    'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80',
+    'https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+  ]
+
   return (
     <DIV>
       <Slide {...slideProperties}>
